23c485e607789ad2b843842f38e83884a7bb4b47
Lee CY <cvb9529@naver.com>
2023-01-09 16:57:32 +0900

    data -> csv

--- code1.py
+++ code1.py
@@ -10,3 +10,6 @@ Iris_Data['target'] = Iris_Data['target'].map({0: "setosa", 1:"versicolor", 2:"v
 
 X_Data = Iris_Data.iloc[:, :-1]
 Y_Data = Iris_Data.iloc[:, [-1]]
+
+"""data -> csv"""
+Iris_Data.to_csv('iris_classification.csv')
